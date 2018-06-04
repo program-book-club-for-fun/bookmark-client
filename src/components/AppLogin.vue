7463761ee85d55967d91515b5b7cfbbfe0748409
ScottHsing <sblgameisfan@gmail,com>
2018-06-04 19:51:09 +0800

    [修改] 1.表單驗證 2.必填檢驗

--- src/components/AppLogin.vue
+++ src/components/AppLogin.vue
@@ -1,42 +1,97 @@
 <template>
-    <v-container>
-          <v-alert :value="alert" color="lime" type="warning">
-            Please fill in Email and Password
-        </v-alert>
-        <v-form @submit.prevent="submit">
-            <v-text-field
-            v-model="email"
-            label="E-mail"
-            required
-            ></v-text-field>
+<v-container>
+  <v-form ref="form" v-model="valid" lazy-validation>
+    <v-text-field
+      v-model="name"
+      :rules="nameRules"
+      :counter="10"
+      label="Name"
+      required
+    ></v-text-field>
+    <v-text-field
+      v-model="email"
+      :rules="emailRules"
+      label="E-mail"
+      required
+    ></v-text-field>
+    <v-select
+      v-model="select"
+      :items="items"
+      :rules="[v => !!v || 'Item is required']"
+      label="Item"
+      required
+    ></v-select>
+    <v-checkbox
+      v-model="checkbox"
+      :rules="[v => !!v || 'You must agree to continue!']"
+      label="Do you agree?"
+      required
+    ></v-checkbox>
 
-            <v-text-field
-            v-model="password"
-            label="Password"
-            type="password"
-            required
-            ></v-text-field>
-            <v-btn color="green" class="success" type="submit">Submit</v-btn>
-        </v-form>    
-    </v-container>
+    <v-btn
+      :disabled="!valid"
+      @click="submit"
+    >
+      submit
+    </v-btn>
+    <v-btn @click="clear">clear</v-btn>
+  </v-form>
+</v-container>
 </template>
 
 <script>
-    export default {
-        data(){
-            return{
-                email:null,
-                password:null,
-                alert:false,
-            }
-        },
-        methods:{
-            submit(){
-                if(!this.email && !this.password){
-                    this.alert = true;
-                }
-                console.log(`Email is ${this.email} and password is ${this.password}`);
-            }
+  import axios from 'axios'
+
+  export default {
+    data: () => ({
+      valid: true,
+      name: '',
+      nameRules: [
+        v => !!v || 'Name is required',
+        v => (v && v.length <= 10) || 'Name must be less than 10 characters'
+      ],
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
+      ],
+      select: null,
+      items: [
+        'Item 1',
+        'Item 2',
+        'Item 3',
+        'Item 4'
+      ],
+      checkbox: false
+    }),
+
+    methods: {
+      submit () {
+        if (this.$refs.form.validate()) {
+          // Native form submission is not yet supported
+          axios.post('/api/submit', {
+            name: this.name,
+            email: this.email,
+            select: this.select,
+            checkbox: this.checkbox
+          })
         }
+      },
+      clear () {
+        this.$refs.form.reset()
+      }
     }
+  }
 </script>
+
+<style>
+.g-signin-button {
+  /* This is where you control how the button looks. Be creative! */
+  display: inline-block;
+  padding: 4px 8px;
+  border-radius: 3px;
+  background-color: #3c82f7;
+  color: #fff;
+  box-shadow: 0 3px 0 #0f69ff;
+}
+</style>
