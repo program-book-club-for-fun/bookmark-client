c9b8e958babd391840f66c357d7b153339fe5fa5
Sunnyhsiao <sunnyhswork@gmail.com>
2018-06-30 22:47:23 +0800

    [fix] Eslint auto fix existing files

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
 // 2018/05/15 Scott added
-import Vuetify from 'vuetify'
+import Vuetify from 'vuetify';
 // index.js or main.js
-import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
-Vue.use(Vuetify)
-Vue.config.productionTip = false
+import 'vuetify/dist/vuetify.min.css'; // Ensure you are using css-loader
+Vue.use(Vuetify);
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
-})
+  components: {App},
+  template: '<App/>',
+});
